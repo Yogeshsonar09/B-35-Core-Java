dbe3bf08a40f0dea2ec544845d566908b87e23e3
HP <Yogsonar1521@gmail.com>
2022-11-17 15:52:03 +0530

    first commit

---
+++ Day _1connectingwithgithub/src/org/tnsindia/gitdemo/demoforgithub.java
@@ -0,0 +1,13 @@
+package org.tnsindia.gitdemo;
+
+
+// Demo for connecting with hub
+public class demoforgithub {
+
+	public static void main(String[] args) {
+		System.out.println("welcom to tns india");
+		System.out.println("Day_1");
+
+	}
+
+}
