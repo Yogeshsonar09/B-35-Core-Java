014e1635def7d265ae8755e18fe46035d821c5f7
HP <yogsonar1521@gmail.com>
2022-11-21 23:43:15 +0530

    frist commit

---
+++ Day_3operator/src/org/tns/jumpingstatement/Brekdemlo.java
@@ -0,0 +1,30 @@
+package org.tns.jumpingstatement;
+
+public class Brekdemlo {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++)
+		{
+
+			if(i==6)
+			{
+				//break;
+				continue;
+			}
+			System.out.println(i+" ");
+
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+}
