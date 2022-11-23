979955bc9d284aac103fb5ecdacfd044f0702c5f
HP <yogsonar1521@gmail.com>
2022-11-23 13:55:06 +0530

    firstt commit

---
+++ Day_4class,object/src/org/tnsindia/classdemo/Class_and_objectdemo.java
@@ -0,0 +1,23 @@
+package org.tnsindia.classdemo;
+import java.util.Scanner;
+
+public class Class_and_objectdemo {
+	//Driver class
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the speed of the car:");
+		int speed=s.nextInt();
+		
+		//creation of object
+		Car obj=new Car();
+		
+		//compile time input
+		//obj.speed=45;
+		obj.speed(speed);
+		s.close();
+
+		
+
+	}
+
+}
---
+++ Day_4class,object/src/org/tnsindia/constructordemo/Constructorexcutor.java
@@ -0,0 +1,21 @@
+package org.tnsindia.constructordemo;
+
+public class Constructorexcutor {
+
+	public static void main(String[] args) {
+		
+		//object creation
+		//object creation
+				Employee e=new Employee();
+				Employee e1=new Employee(25000,"Nikita");
+				Employee e2=new Employee(45000,"Chaitali");
+
+				e1.print();
+				e2.print();
+
+	
+
+
+	}
+
+}
