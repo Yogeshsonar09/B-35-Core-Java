f74bc9a11a4adf7c138ff779b8307b8da9d94cf4
HP <yogsonar1521@gmail.com>
2022-11-28 12:24:07 +0530

    first commit

--- Day_3operator/src/org/tnsindia/looping/Forloop.java
+++ Day_3operator/src/org/tnsindia/looping/Forloop.java
@@ -7,7 +7,7 @@ public class Forloop {
 	public static void main(String[] args) {
 		Scanner s =new Scanner(System.in);
 		int n =s.nextInt();
-		for(int i=1;i<=n;i++)
+		for(int i=n;i<=1000;i++)
 		{
 			System.out.println(i +" ");
 			s.close();
