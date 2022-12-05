6934187e9b4dc6a06e279ec47ef5fb82555f6c51
HP <yogsonar1521@gmail.com>
2022-12-05 11:44:11 +0530

    first commit

---
+++ Day-10_Polymorphism_InterfaceAnd_WrapperClasses/src/org/tnsindia/interfacedemo/HR.java
@@ -0,0 +1,10 @@
+
+
+
+package org.tnsindia.interfacedemo;
+//INTERFACE 1
+public interface HR {
+	//abstract method
+	void salaryDiscussion();
+
+}
---
+++ Day-10_Polymorphism_InterfaceAnd_WrapperClasses/src/org/tnsindia/polymorphismdemo/MethodOverridingChild.java
@@ -0,0 +1,15 @@
+
+
+package org.tnsindia.polymorphismdemo;
+
+//child class
+//Use inheritance for overriding
+public class MethodOverridingChild extends MethodOverridingExample {
+	public int mul(int a,int b)
+	{
+		return a*b;
+		
+	}
+
+	
+}
